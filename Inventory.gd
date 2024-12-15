503b57f00f0acc6cd518758228b1f155f26ca058
InsurgenceGaming <monstergaming@outlook.com>
2024-12-15 15:48:10 +0000

    Get_parent removal.

--- Inventory.gd
+++ Inventory.gd
@@ -3,17 +3,22 @@ extends Control
 var mouse_in = false
 @onready var slot = preload("res://UI/Slot.tscn")
 # Called when the node enters the scene tree for the first time.
-
-#
+var player : Node
+func initialize(player_ref: Node):
+	player = player_ref
+	
+	
 func _ready():
 	if $Panel/GridContainer.get_children().is_empty():
-		for i in get_parent().get_parent().get_meta("Inventory_size"):
+		for i in player.get_meta("Inventory_size"):
 			var new_slot = slot.instantiate()
+			new_slot.initialize(player,$".")
 			$Panel/GridContainer.add_child(new_slot)
 		
 	var slots: Array = $Panel/GridContainer.get_children()
-	for i in range(min(get_parent().get_parent().get_meta("Inventory_size").size(),slots.size())):
-		slots[i].Slot_update(get_parent().get_parent().get_meta("Inventory_size")[i])
+	for i in range(min(player.get_meta("Inventory_size").size(),slots.size())):
+		slots[i].Slot_update(player.get_meta("Inventory_size")[i])
+		
 		
 	
 			
@@ -29,7 +34,15 @@ func _input(event):
 			print("yo")
 
 func _on_button_pressed():
+<<<<<<< Updated upstream
 	get_parent().get_parent().set_meta("inventory_open",false)
+=======
+	player.set_meta("inventory_open",false)
+	if get_meta("ItemHeld") == true:
+		Input.set_custom_mouse_cursor(null)
+		set_meta("ItemHeld",false)
+		print(get_meta("ItemHeld"))
+>>>>>>> Stashed changes
 	self.queue_free()
 	
 
--- Slot.gd
+++ Slot.gd
@@ -1,6 +1,14 @@
 extends Panel
 
 @onready var item_visual : Sprite2D = $Sprite2D
+<<<<<<< Updated upstream
+=======
+var player : Node
+var inventory_Base_node : Node
+func initialize(player_ref: Node, inventory_ref : Node):
+	player = player_ref
+	inventory_Base_node = inventory_ref
+>>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,7 +16,6 @@ func _ready():
 func Slot_update(item:InvItem):
 	if item:
 		item_visual.texture = item.Item_Texture
-		print("i ran")
 		if item.Item_quantity > 1:
 			$Label.visible = true
 			$Label.text = str(item.Item_quantity)
@@ -16,7 +23,54 @@ func Slot_update(item:InvItem):
 			$Label.visible = false
 	else:
 		pass
+<<<<<<< Updated upstream
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+=======
+		
+func _on_gui_input(event: InputEvent) -> void:
+	##if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("move_item"):
+		var index = get_index_in_grid()
+		var inventory = player.inventory_array
+		
+		if inventory_Base_node.has_meta("ItemHeld") and inventory_Base_node.get_meta("ItemHeld"):
+			# Item is being held, so swap the last clicked slot with the current one
+			var last_clicked_slot = inventory_Base_node.get_meta("last_clicked_slot")
+			if last_clicked_slot != null:
+				# Swap the two items in the inventory array
+				var temp = inventory[index]
+				inventory[index] = inventory[last_clicked_slot]
+				inventory[last_clicked_slot] = temp
+
+				# Update the UI for both slots
+				Slot_update(inventory[index])
+				get_parent().get_child(last_clicked_slot).Slot_update(inventory[last_clicked_slot])
+
+				# Reset the item held metadata
+				inventory_Base_node.set_meta("ItemHeld", false)
+				Input.set_custom_mouse_cursor(null)  # Reset cursor to default
+				print("Swapped items between slots", last_clicked_slot, "and", index)
+		else:
+			# No item is being held, pick up the item in the current slot
+			print("slot_clicked", index)
+			print(inventory[index].Item_name)
+			
+			Input.set_custom_mouse_cursor(inventory[index].Item_Texture)
+			item_visual.texture = null
+			$Label.visible = false
+
+			# Set the metadata to indicate an item is being held
+			inventory_Base_node.set_meta("ItemHeld", true)
+			inventory_Base_node.set_meta("last_clicked_slot", index)
+			print("Item picked up from slot", index)
+
+func get_index_in_grid() -> int:
+	var parent_grid = get_parent()  # This should be the GridContainer
+	for i in range(parent_grid.get_child_count()):
+		if parent_grid.get_child(i) == self:
+			return i
+	return -1
+>>>>>>> Stashed changes
